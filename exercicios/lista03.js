4cb41662b03f5d810e80624ecb0a2686835a731c
Jailson S <dev.jailson@gmail.com>
2023-03-30 14:53:40 -0300

    Síntaxe da lista de exercicio 03

---
+++ exercicios/lista03.js
@@ -0,0 +1,10 @@
+/* Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar a o cálculo adequado.
+
+Código Condição de pagamento:
+- À vista Débito, recebe 10% de desconto;
+- À vista no Dinheiro ou PIX, recebe 15% de desconto;
+- Em duas vezes, preço normal de etiqueta sem juros;
+- Acima de duas vezes, preco normal de etiqueta mais juros de 10%;
+
+*/
