ad876880537162c7806c46f4e0fd422389aaa815
Jailson S <dev.jailson@gmail.com>
2023-03-31 11:48:43 -0300

    Realização da lista de exercício 3

--- exercicios/lista03.js
+++ exercicios/lista03.js
@@ -2,9 +2,27 @@
 Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar a o cálculo adequado.
 
 Código Condição de pagamento:
-- À vista Débito, recebe 10% de desconto;
-- À vista no Dinheiro ou PIX, recebe 15% de desconto;
-- Em duas vezes, preço normal de etiqueta sem juros;
-- Acima de duas vezes, preco normal de etiqueta mais juros de 10%;
+1- À vista Débito, recebe 10% de desconto;
+2- À vista no Dinheiro ou PIX, recebe 15% de desconto;
+3- Em duas vezes, preço normal de etiqueta sem juros;
+4- Acima de duas vezes, preco normal de etiqueta mais juros de 10%;
 
 */
+
+const precoEtiqueta = 100;
+const formaDePagamento = 01;
+
+if (formaDePagamento === 1){
+    const precoFinal = precoEtiqueta - (precoEtiqueta * 0.1);
+    console.log('A forma de pagamento escolhida foi À vista Débito, e o valor final da compra foi de R$'+ precoFinal.toFixed(2));
+} else if (formaDePagamento === 2){
+    const precoFinal = precoEtiqueta - (precoEtiqueta * 0.15);
+    console.log('A forma de pagamento escolhida foi À vista no Dinheiro ou PIX, e o valor final da compra foi de R$'+ precoFinal.toFixed(2));
+} else if (formaDePagamento === 3) {
+    console.log('A forma de pagamento escolhida foi parcelado em duas vezes, e o valor final da compra foi de R$'+ precoEtiqueta.toFixed(2));
+} else if (formaDePagamento === 4) {
+    const precoFinal = precoEtiqueta + (precoEtiqueta * 0.10);
+    console.log('A forma de pagamento escolhida foi Parcelado acima de duas vezes, e o valor final da compra foi de R$'+ precoFinal.toFixed(2));
+} else {
+    console.log('Forma de pagamento inválida');
+}
