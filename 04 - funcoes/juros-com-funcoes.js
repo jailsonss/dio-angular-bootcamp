3d750713ddb49ec49336ea1bf2a969b90efed3cc
Jailson S <dev.jailson@gmail.com>
2023-04-07 16:35:55 -0300

    Cálculo de Juros e Descontos com functions

--- 04 - funcoes/juros-com-funcoes.js
+++ 04 - funcoes/juros-com-funcoes.js
@@ -8,26 +8,26 @@ Código Condição de pagamento:
 4- Acima de duas vezes, preco normal de etiqueta mais juros de 10%;
 
 */
-function aplicarDesconto(valor, deconto){
-    return valor - (valor * desconto);
+function aplicarDesconto(valor, desconto){
+    return valor - (valor * (desconto / 100));
+}
+
+function aplicarJuros(valor, juros){
+    return valor + (valor * (juros / 100));
 }
 
 const precoEtiqueta = 100;
-const formaDePagamento = 1;
+const formaDePagamento = 5;
 
-function pagamento(){
-    if (formaDePagamento === 1){
-        const precoFinal = precoEtiqueta - (precoEtiqueta * 0.1);
-        return 'A forma de pagamento escolhida foi À vista Débito, e o valor final da compra foi de R$'+ precoFinal.toFixed(2);
-    } else if (formaDePagamento === 2){
-        const precoFinal = precoEtiqueta - (precoEtiqueta * 0.15);
-        return 'A forma de pagamento escolhida foi À vista no Dinheiro ou PIX, e o valor final da compra foi de R$'+ precoFinal.toFixed(2);
-    } else if (formaDePagamento === 3) {
-        return 'A forma de pagamento escolhida foi parcelado em duas vezes, e o valor final da compra foi de R$'+ precoEtiqueta.toFixed(2);
-    } else if (formaDePagamento === 4) {
-        const precoFinal = precoEtiqueta + (precoEtiqueta * 0.10);
-        return 'A forma de pagamento escolhida foi Parcelado acima de duas vezes, e o valor final da compra foi de R$'+ precoFinal.toFixed(2);
-    } else {
-        return 'Forma de pagamento inválida';
-    }
+
+if (formaDePagamento === 1){
+    console.log ('A forma de pagamento escolhida foi À vista Débito, e o valor final da compra foi de R$'+ aplicarDesconto(precoEtiqueta, 10).toFixed(2));
+} else if (formaDePagamento === 2){
+    console.log('A forma de pagamento escolhida foi À vista no Dinheiro ou PIX, e o valor final da compra foi de R$'+ aplicarDesconto(precoEtiqueta, 15).toFixed(2))
+} else if (formaDePagamento === 3) {
+    console.log ('A forma de pagamento escolhida foi parcelado em duas vezes, e o valor final da compra foi de R$'+ precoEtiqueta.toFixed(2));
+} else if (formaDePagamento === 4) {
+    console.log('A forma de pagamento escolhida foi Parcelado acima de duas vezes, e o valor final da compra foi de R$'+ aplicarJuros(precoEtiqueta, 10).toFixed(2));
+} else {
+    console.log('Forma de pagamento inválida');
 }
