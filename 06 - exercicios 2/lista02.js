11b4df73ab5f1246367f2160bec5b3c135cece8f
Jailson S <dev.jailson@gmail.com>
2023-04-08 14:35:09 -0300

    Funções e Objetos - Lista 02

---
+++ 06 - exercicios 2/lista02.js
@@ -0,0 +1,46 @@
+/*
+    2) Crie uma classe para representar pessoas.
+    Para cada pessoa teremos os atributos nome, peso e altura.
+    As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
+    Istancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor do seu IMC;
+*/
+
+class Pessoa{
+    nome;
+    peso;
+    altura;
+
+    constructor (nome, peso, altura){
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    calculoImc (peso, altura){
+       return this.peso / Math.pow(this.altura,2);
+    }
+
+    classificarImc(){
+        const imc = this.calculoImc();
+
+        if (imc < 18.5){
+            return 'Seu IMC é '+ imc.toFixed(2) +'. Logo, você está ABAIXO DO PESO.';
+        } else if (imc >= 18.5 && imc < 25) {
+            return 'Seu IMC é '+ imc.toFixed(2) +'. Logo, você está com PESO NORMAL.';
+        } else if (imc >= 25 && imc < 30) {
+            return 'Seu IMC é '+ imc.toFixed(2) +'. Logo, você está ACIMA DO PESO.';
+        } else if (imc >= 30 && imc <= 40) {
+            return 'Seu IMC é '+ imc.toFixed(2) +'. Logo, você está OBESO.';
+        } else {
+            return 'Seu IMC é '+ imc.toFixed(2) +'. Logo, você está com PESO NORMAL.';
+        }
+    }
+}
+
+const jose = new Pessoa('José', 70, 1.75);
+
+// Mostra o resultado do IMC
+console.log(jose.calculoImc().toFixed(2));
+
+// Mostra a classificação a partir do IMC calculado
+console.log(jose.classificarImc());
